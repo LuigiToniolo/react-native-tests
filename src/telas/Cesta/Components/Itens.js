496273b48b984ebc72ec7ac03e8512afd0c7e111
LuigiToniolo <luigi.toniolo@domvsit.com.br>
2022-03-11 10:36:23 -0300

    commitando final do curso

--- src/telas/Cesta/Components/Itens.js
+++ src/telas/Cesta/Components/Itens.js
@@ -7,14 +7,16 @@ export default function Itens({ titulo, lista }) {
     return (
     <>
         <Texto style={estilos.titulo}>{titulo}</Texto>
-        { lista.map((nome, imagem) => {
+        {lista.map(({nome, imagem}) => {
             return (
-            <View>
-                <Texto style={estilos.item}>{ nome }</Texto>
-                <Image source={imagem}/>
-            </View>
+                    <View key={nome} style={estilos.item}>
+                        <Image style={estilos.imagem} source={imagem} />
+                        <Texto style={estilos.nome}>{ nome }</Texto>
+                    </View>
             );
         }) }
+
+        
     </>
     );
 }
@@ -29,6 +31,21 @@ const estilos = StyleSheet.create({
         lineHeight: 32,
     },
     item: {
-
+        flexDirection: "row",
+        borderBottomWidth: 1, 
+        borderBottomColor: "#ECECEC",
+        paddingVertical: 16,
+        alignItems: "center",
+        fontWeight: "bold",
+    },
+    imagem: {
+        width: 46,
+        height: 46,
+    },
+    nome: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 11,
+        color: "#464646"
     }
 })
